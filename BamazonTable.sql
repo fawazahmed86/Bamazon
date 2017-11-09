e899e2757d93b49f6f85ece31ddc69f2a3af7fc7
fawazahmed86 <honorfas@gmail.com>
2017-11-09 10:03:17 -0800

    Update BamazonTable.sql

--- BamazonTable.sql
+++ BamazonTable.sql
@@ -6,14 +6,20 @@ USE Bamazon;
 
 CREATE table Products(
 ItemID INTEGER(30) AUTO_INCREMENT NOT NULL,
-ProductName VARCHAR(30) NOT NULL,
-DepartmentName VARCHAR(30) NOT NULL,
+Item VARCHAR(30) NOT NULL,
+DeptName VARCHAR(30) NOT NULL,
 Price DECIMAL(10,2) NOT NULL,
-StockQuantity INTEGER(10) NOT NULL,
+Quantity INTEGER(10) NOT NULL,
 PRIMARY KEY(ItemID)
 );
 
-INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("bread", "bakery", 2.50, 5);
-INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("banana", "fruits & veggies", 1.5, 20);
-INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("apple", "fruits & veggies", .99, 30);
-INSERT INTO products (ProductName, DepartmentName, Price, StockQuantity) VALUES ("grapes", "fruits & veggies", 3.5, 15);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("4G Router", "Mobile Devices", 349.0, 15);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("Smasumg Note 8", "Mobile Devices", 849, 20);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("A4 Paper Sets", "stationary", 3.49, 40);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("Lined loose sheets", "stationary", 2.35, 25);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("Ear Plugs", "Accessories", 4.35, 50);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("Huawei Honor 5X", "Mobile Devices", 459, 15);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("LG 5X", "Mobile Devices", 339, 10);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("HTC Desire 10 Pro", "Mobile Devices", 869, 6);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("HTC Desire 10 Compact", "Mobile Devices", 729, 8);
+INSERT INTO products (Item, DeptName, Price, Quantity) VALUES ("HTC Desire 650", "Mobile Devices", 229, 15);
